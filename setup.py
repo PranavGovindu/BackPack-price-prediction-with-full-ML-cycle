7466cc202de8964dd19278208d4ff036119ef8b6
PranavGovindu <violaze25@gmail.com>
2025-03-31 12:28:52 +0530

    project structure and dvc initialized

---
+++ setup.py
@@ -0,0 +1,11 @@
+# from setuptools import setup, find_packages
+
+# setup(
+#     name="st_1",  # Choose a valid package name (avoid "src")
+#     packages=find_packages(where="src"),  # Looks inside 'src/' for sub-packages
+#     package_dir={"": "src"},  # Maps 'src/' as the root package
+#     version="0.1.0",
+#     description="st_1 package",
+#     author="Pranav",
+#     license="MIT",
+# )
